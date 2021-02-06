cd8e54f86e1f131caf2b73fdeb7fa1504de88120
EC2 Default User <ec2-user@ip-172-31-19-116.us-west-1.compute.internal>
2021-02-06 09:13:16 +0000

    add project details along added url

--- client/src/config_sample.js
+++ client/src/config_sample.js
@@ -9,8 +9,8 @@ export default {
 		APP_CLIENT_ID: '7t7a04dvc3ag6eiuj2jqj7ndu2',
 		DOMAIN: 'https://wz2sdbtt54.auth.us-west-1.amazoncognito.com',
 		SCOPE: ['email', 'openid'],
-		REDIRECT_SIGN_IN: 'YOUR_URL_FOR_SIGN_IN',
-		REDIRECT_SIGN_OUT: 'YOUR_URL_FOR_SIGN_IN',
+		REDIRECT_SIGN_IN: 'http://ec2-13-56-101-248.us-west-1.compute.amazonaws.com:3000',
+		REDIRECT_SIGN_OUT: 'http://ec2-13-56-101-248.us-west-1.compute.amazonaws.com:3000',
 		RESPONSE_TYPE: 'code'
 	}
 };
