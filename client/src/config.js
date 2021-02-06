8e768c02b58fd394d90799d921dcbbeabb8cc4ba
EC2 Default User <ec2-user@ip-172-31-19-116.us-west-1.compute.internal>
2021-02-06 09:15:40 +0000

    add project details along added url

--- client/src/config.js
+++ client/src/config.js
@@ -1,16 +1,16 @@
 export default {
 	apiGateway: {
-		REGION: 'YOUR_API_GATEWAY_REGION',
-		URL: 'YOUR_API_GATEWAY_URL'
+		REGION: 'us-west-1',
+		URL: 'https://wz2sdbtt54.execute-api.us-west-1.amazonaws.com/dev/'
 	},
 	cognito: {
-		REGION: 'YOUR_COGNITO_REGION',
-		USER_POOL_ID: 'YOUR_COGNITO_USER_POOL_ID',
-		APP_CLIENT_ID: 'YOUR_COGNITO_APP_CLIENT_ID',
-		DOMAIN: 'YOUR_COGNITO_DOMAIN',
+		REGION: 'us-west-1',
+		USER_POOL_ID: 'us-west-1_7T6ej5aig',
+		APP_CLIENT_ID: '7t7a04dvc3ag6eiuj2jqj7ndu2',
+		DOMAIN: 'https://wz2sdbtt54.auth.us-west-1.amazoncognito.com',
 		SCOPE: ['email', 'openid'],
-		REDIRECT_SIGN_IN: 'YOUR_URL_FOR_SIGN_IN',
-		REDIRECT_SIGN_OUT: 'YOUR_URL_FOR_SIGN_IN',
+		REDIRECT_SIGN_IN: 'http://ec2-13-56-101-248.us-west-1.compute.amazonaws.com:3000',
+		REDIRECT_SIGN_OUT: 'http://ec2-13-56-101-248.us-west-1.compute.amazonaws.com:3000',
 		RESPONSE_TYPE: 'code'
 	}
 };
